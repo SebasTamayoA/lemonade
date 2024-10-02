dee40a9c3fdd02a0b412907b2534cb9bfb60d118
Sebastian Tamayo <sebastamayo37@gmail.com>
2024-10-01 19:12:04 -0500

    Modificaciones al main

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,6 +41,12 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
     var currentStep by remember { mutableStateOf(1) }
     when (currentStep) {
         1 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon1,
+                drawableResourceId = R.drawable.lemon_tree,
+                contentDescription = R.string.lemon1,
+                onImageClick = { currentStep = 2 }
+            )
             Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
             {
                 Spacer(modifier = Modifier.height(16.dp))
@@ -52,6 +61,12 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
             }
         }
         2 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon2,
+                drawableResourceId = R.drawable.lemon_squeeze,
+                contentDescription = R.string.lemon2,
+                onImageClick = { currentStep = 3 }
+            )
             Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
             {
                 Spacer(modifier = Modifier.height(16.dp))
@@ -64,6 +79,12 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
             }
         }
         3 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon3,
+                drawableResourceId = R.drawable.lemon_drink,
+                contentDescription = R.string.lemon3,
+                onImageClick = { currentStep = 4 }
+            )
             Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
             {
                 Spacer(modifier = Modifier.height(16.dp))
@@ -76,6 +97,12 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
             }
         }
         4 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.lemon4,
+                drawableResourceId = R.drawable.lemon_restart,
+                contentDescription = R.string.lemon4,
+                onImageClick = { currentStep = 1 }
+            )
             Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
             {
                 Spacer(modifier = Modifier.height(16.dp))
@@ -89,6 +116,35 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
         }
     }
 
+}
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId : Int,
+    drawableResourceId : Int,
+    contentDescription : Int,
+    onImageClick : () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Box(modifier = modifier){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(onClick = onImageClick,
+                shape = roundedCornerShape(
+                    dimensionResource(id = R.dimen.button_corner_radius),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.ternary),
+            ) {
+                Image(
+                    painter = painterResource(drawableResourceId),
+                    contentDescription = stringResource(contentDescription),
+                    modifier = Modifier.size(200.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview
